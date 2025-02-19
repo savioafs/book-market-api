4c6ddf2280d1623bed0faa8412911cae7b437286
Sávio Alexandre <savioalefreitas@gmail.com>
2025-02-19 14:56:02 -0300

    add service monitor expiration time coupons

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/savioafs/book-market/internal/config"
 	"github.com/savioafs/book-market/internal/database"
 	"github.com/savioafs/book-market/internal/server"
+	"github.com/savioafs/book-market/internal/service"
 	"log"
 )
 
@@ -14,6 +15,8 @@ func main() {
 	if err != nil {
 		log.Fatal("fail to init database", err)
 	}
+
+	go service.MonitorCouponsByExpirationDate(db)
 
 	sv := server.NewServer(":8080", db)
 	sv.Run()
---
+++ internal/service/discountCoupon_service.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"github.com/savioafs/book-market/internal/repository"
+	"gorm.io/gorm"
+	"log"
+	"time"
+)
+
+func MonitorCouponsByExpirationDate(db *gorm.DB) {
+	for {
+
+		time.Sleep(1 * time.Second)
+
+		discountCouponsRepository := repository.NewDiscountCouponRepositoryGorm(db)
+
+		coupons, err := discountCouponsRepository.GetActiveDiscountsCoupons()
+		if err != nil {
+			log.Printf("fail to get active discount coupons: %s", err.Error())
+			continue
+		}
+
+		for _, coupon := range *coupons {
+			if coupon.ExpirationDate.Before(time.Now()) {
+				err = discountCouponsRepository.DisableDiscountCoupon(coupon.ID)
+				if err != nil {
+					log.Printf("fail to discount coupon: %s", err.Error())
+					continue
+				}
+			}
+		}
+	}
+}
